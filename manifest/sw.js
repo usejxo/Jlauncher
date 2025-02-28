d2342ee99e8444424107bc78a3c38a6cebec1c51
robotjxo <164087279+robotjxo@users.noreply.github.com>
2025-02-28 00:08:18 -0600

    Update PWA

--- manifest/sw.js
+++ manifest/sw.js
@@ -1,5 +1,5 @@
 // Name of the cache
-const CACHE_NAME = 'ccc';
+const CACHE_NAME = 'Jlauncher-cache-V1';
 
 // Install event: Cache all files in the specified list during the install phase
 self.addEventListener('install', (event) => {
